b23e0e34cca199ace71d7da179a4157758a26ed0
Jonas Möller <moeller-jonas@web.de>
2016-12-29 19:51:24 +0100

    Release to github

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,10 +2,10 @@ import { browser, element, by } from 'protractor';
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'Hello Angular';
+	let expectedMsg = 'Hello Angular';
 
-  beforeEach(function () {
-	  browser.get('');
+	beforeEach(function () {
+		browser.get('');
 	});
 
 	it('should display: ' + expectedMsg, function () {
